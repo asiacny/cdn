6cbc1dde8402b403d5ff46573adf5d1c87316dac
asiacny <asiacny@gmail.com>
2019-08-30 05:10:16 +0800

    201908300510

--- js/data.js
+++ js/data.js
@@ -421,7 +421,7 @@ function html_xr(data_item){
       var now=new Date();
       var runtime2 = performance.now();
       $(".nano").nanoScroller();
-      console.log("滚动条样式加载完成","耗时:" + (performance.now() - runtime2).toFixed(2) + "ms");
+      console.log("各种接口定制，详询api#ml.mk  #=@","","","滚动条样式加载完成","耗时:" + (performance.now() - runtime2).toFixed(2) + "ms", "总耗时:" + performance.now().toFixed(2) + "ms");
     }
   })
 };
