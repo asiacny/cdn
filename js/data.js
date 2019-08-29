f1bc3cba0bf52d1180151c701f653571f4bf53e8
asiacny <asiacny@gmail.com>
2019-08-30 05:21:16 +0800

    201908300521

--- js/data.js
+++ js/data.js
@@ -417,7 +417,7 @@ function html_xr(data_item){
     }
     console.log("各种接口定制，详询api#ml.mk  #=@","","","（" + data_all['title'] + "）加载完成", "耗时:" + (performance.now() - runtime).toFixed(2) + "ms", "总耗时:" + performance.now().toFixed(2) + "ms");
     ii++;
-    if(ii=34){
+    if(ii==34){
       var now=new Date();
       var runtime2 = performance.now();
       $(".nano").nanoScroller();
